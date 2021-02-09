cff1a1d848ad23170c28745409fe94f689e06b09
leila-boumediene <lilabdm@gmail.com>
2021-02-09 14:15:29 +0100

    avancement

--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const Menu = ({ data }) => {
+  const [counter, setCounter] = useState(0);
   console.log(data);
   return (
     <>
@@ -8,7 +11,13 @@ const Menu = ({ data }) => {
             <div
               className="menu"
               onClick={() => {
-                alert(item.id);
+                item.title && setCounter(counter + 1);
+                return (
+                  <>
+                    <h3>{item.title}</h3>
+                    <span>{item.price}</span>
+                  </>
+                );
               }}
               key={item.id}
             >
@@ -24,6 +33,37 @@ const Menu = ({ data }) => {
       <div className="panier">
         <button type="button">Valider mon panier</button>
         <p>Votre panier est vide</p>
+        <div
+          className="add"
+          onClick={() => {
+            <button type="button">Ajouter au panier</button>;
+          }}
+        ></div>
+        <div
+          className="delet"
+          onClick={() => {
+            <button type="button">Vider mon panier</button>;
+          }}
+        ></div>
+      </div>
+      <div className="quantity">
+        <button
+          className="downQuantity"
+          onClick={() => {
+            setCounter(counter - 1);
+          }}
+        >
+          -
+        </button>
+        <p>{counter}</p>
+        <button
+          className="upQuantity"
+          onClick={() => {
+            setCounter(counter + 1);
+          }}
+        >
+          +
+        </button>
       </div>
     </>
   );
