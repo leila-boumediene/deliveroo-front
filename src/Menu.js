b8dcd5468590d869bafb476c67002819e2ab643c
leila-boumediene <lilabdm@gmail.com>
2021-02-09 14:57:29 +0100

    modification

--- src/Menu.js
+++ src/Menu.js
@@ -2,68 +2,72 @@ import { useState } from "react";
 
 const Menu = ({ data }) => {
   const [counter, setCounter] = useState(0);
+  const handleClick = () => {
+    setCounter((counter = 0));
+  };
   console.log(data);
   return (
     <>
-      <div>
-        {data.map((item, index) => {
-          return (
-            <div
-              className="menu"
-              onClick={() => {
-                item.title && setCounter(counter + 1);
-                return (
-                  <>
-                    <h3>{item.title}</h3>
-                    <span>{item.price}</span>
-                  </>
-                );
-              }}
-              key={item.id}
-            >
-              <h3>{item.title}</h3>
-              <p>{item.description}</p>
-              <span>{item.price}</span>
-              <img src={item.picture} alt={item.title} />
-            </div>
-          );
-        })}
-      </div>
+      <div className="main">
+        <div>
+          {data.map((item, index) => {
+            return (
+              <div
+                className="menu"
+                onClick={() => {
+                  item.title && setCounter(counter + 1);
+                  console.log(item.title, item.price);
 
-      <div className="panier">
-        <button type="button">Valider mon panier</button>
-        <p>Votre panier est vide</p>
-        <div
-          className="add"
-          onClick={() => {
-            <button type="button">Ajouter au panier</button>;
-          }}
-        ></div>
-        <div
-          className="delet"
-          onClick={() => {
-            <button type="button">Vider mon panier</button>;
-          }}
-        ></div>
-      </div>
-      <div className="quantity">
-        <button
-          className="downQuantity"
-          onClick={() => {
-            setCounter(counter - 1);
-          }}
-        >
-          -
-        </button>
-        <p>{counter}</p>
-        <button
-          className="upQuantity"
-          onClick={() => {
-            setCounter(counter + 1);
-          }}
-        >
-          +
-        </button>
+                  <p>{(item.title, item.price)}</p>;
+                }}
+                key={item.id}
+              >
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+                <span>{item.price}</span>
+                <img src={item.picture} alt={item.title} />
+              </div>
+            );
+          })}
+
+          <div className="panier">
+            <button type="button">Valider mon panier</button>
+            <p>Votre panier est vide</p>
+            <div
+              className="add"
+              onClick={() => {
+                <button type="button">Ajouter au panier</button>;
+              }}
+            ></div>
+            <div>
+              <button className="delet" onCLick={handleClick}>
+                Vider mon panier
+              </button>
+            </div>
+
+            <div className="allButtons">
+              <div className="quantity">
+                <button
+                  className="downQuantity"
+                  onClick={() => {
+                    setCounter(counter - 1);
+                  }}
+                >
+                  -
+                </button>
+                <p>{counter}</p>
+                <button
+                  className="upQuantity"
+                  onClick={() => {
+                    setCounter(counter + 1);
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
